f4a594ca125adda1a25bb1a65c6bfae4f97d6a74
sandip28sutar <sandipsutar2810@gmail.com>
2026-08-24 18:07:12 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = "sutar-erp-v1";
+const ASSETS = ["./", "./index.html", "./header.jpg", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
